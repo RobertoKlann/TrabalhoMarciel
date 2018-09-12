9c4be9ec136a1e57cbb975932274ac73c6d5f0ba
Roberto Oswaldo Klann <40249@UNIDAVI.LOCAL>
2018-09-12 20:37:20 -0300

    SQL

---
+++ tbturmaaluno.sql
@@ -0,0 +1,49 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 13-Set-2018 às 01:36
+-- Versão do servidor: 10.1.31-MariaDB
+-- PHP Version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `escola`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tbturmaaluno`
+--
+
+CREATE TABLE `tbturmaaluno` (
+  `alucodigo` int(11) NOT NULL,
+  `turcodigo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbturmaaluno`
+--
+ALTER TABLE `tbturmaaluno`
+  ADD PRIMARY KEY (`alucodigo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
